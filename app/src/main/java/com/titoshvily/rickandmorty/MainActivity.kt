b54f2a54f2af2e827c266572ca7b191f3c0e42d7
ivant <ivan.titov093.093@gmail.com>
2025-11-04 14:22:52 +0300

    Search bar, update viewModel

--- app/src/main/java/com/titoshvily/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/titoshvily/rickandmorty/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.titoshvily.rickandmorty.presentation.composable.CharacterList
 import com.titoshvily.rickandmorty.presentation.composable.LoadScreen
 import com.titoshvily.rickandmorty.presentation.composable.SearchBar
@@ -52,6 +54,7 @@ class MainActivity : ComponentActivity() {
                             onSearch = { query -> characterViewModel.loadSearchCharacters(query) },
                             onClear = { characterViewModel.clearSearch() }
                         )
+                        Spacer(modifier = Modifier.padding(2.dp))
 
 
                         val displayList = if (isSearchMode) searchResults else characters
