d2bc0c1e8d356fc19b39d93e9593ba67d9191d20
ivant <ivan.titov093.093@gmail.com>
2025-11-07 16:03:16 +0300

    version 1.1

--- app/src/main/java/com/titoshvily/rickandmorty/presentation/composable/CharacterDetailScreen.kt
+++ app/src/main/java/com/titoshvily/rickandmorty/presentation/composable/CharacterDetailScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
